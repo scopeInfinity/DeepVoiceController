b7295d0fba71c54a601d27bfeea71c40f9db4e4f
Gagan Kumar <gagan1kumar.cs@gmail.com>
2018-03-12 16:35:23 +0530

    Minimal Structure for setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='deepvoicecontroller',
+      version='0.1',
+      description='Controls Linux Destro using Voice Commands',
+      url='https://github.com/scopeInfinity/DeepVoiceController',
+      author='Gagan Kumar, Shifona Garg',
+      author_email='gagan1kumar.cs@gmail.com, shivanikrishgarg@gmail.com',
+      packages=['deepvoicecontroller'],
+      python_requires='>=2.6, <3',
+      zip_safe=False)
