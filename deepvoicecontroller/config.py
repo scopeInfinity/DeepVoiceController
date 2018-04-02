a411e6a06c5b781f1778c1c6b6b6f764f5134d14
Shifona Garg <shivanikrishgarg@gmail.com>
2018-04-02 22:38:33 +0530

    Config, find latest model

--- deepvoicecontroller/config.py
+++ deepvoicecontroller/config.py
@@ -11,4 +11,8 @@ class Config():
         return os.path.join(self.getHome(), "dataset")
 
     def getModelFname(self):
-    	return os.path.join(self.getHome(), "model")
+    	w_dir = os.path.join(self.getHome(), "Weights")
+    	list_of_files=[os.path.join(w_dir, x) for x in os.listdir(w_dir)]
+        latest_file = max(list_of_files, key=os.path.getctime)
+        print("Using %s as model"%latest_file)
+        return latest_file
