9491120c709bf2639a812be33b0e6645ba374985
Shifona Garg <shivanikrishgarg@gmail.com>
2018-04-07 23:41:11 +0530

    Added save Image function and Event Handler functionalities

--- deepvoicecontroller/main.py
+++ deepvoicecontroller/main.py
@@ -3,8 +3,14 @@ import sys
 
 from config import Config
 from datahandler import DataHandler
-from model import Model
 from word_parser import word_parser
+
+def save_img(fname,tfname):
+    from audio import load_and_preprocess_audio
+    from PIL import Image
+    mat= load_and_preprocess_audio(fname, False)
+    img = Image.fromarray(mat, 'L')
+    img.save(tfname)
 
 def main():
     config = Config()
@@ -12,23 +18,31 @@ def main():
     parser.add_argument('-td', '--test-dataset', help='Walk through dataset \
         and test while preprocessing', action='store_true')
     parser.add_argument('-e', '--execute', help='Execute',  action='store_true')
+    parser.add_argument('-d', '--dry', help='Dry Run',  action='store_true')
     parser.add_argument('-t', '--train', help='Train Model', action='store_true')
     parser.add_argument('-wp', '--word-parser', help='Listen to microphone parse the word', action='store_true')
     parser.add_argument('-p', '--predict', help='Predict Audiofile', nargs='+')
+    parser.add_argument('-si', '--save_image', help='Save Image from wav')
+    parser.add_argument('-ti', '--target_image', help='Save Image target file')
+
    
     args = parser.parse_args()
     if args.test_dataset:
         datahandler = DataHandler()
         print("Test Passed")
         return
+    if args.save_image and args.target_image:
+        save_img(args.save_image,args.target_image)
     if args.execute:
         from event_handler import EventHandler
-        eh = EventHandler()
+        eh = EventHandler(args.dry)
         word_parser(eh)
     if args.train:
+        from model import Model
         model = Model()
         model.train()
     if args.predict:
+        from model import Model
         model=Model()
         datahandler = DataHandler(noActualLoad = True)
         result = model.predict(args.predict, datahandler.getClasses(),whole = True)
